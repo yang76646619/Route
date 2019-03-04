36ca3ca58d8c77f04ea790b3594ecf180aaa2bea
yang76646619 <yang76646619@163.com>
2019-03-04 21:58:11 +0800

    Graphhopper

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include<algorithm>
+#include <time.h>
+using namespace std;
+
+
+int main()
+{
+	//启动后台服务 调用jar包 
+	system("java -Xmx2g -Xms2g -Dgraphhopper.datareader.file=..\\beijing2.osm -jar ..\\graphhopper-web-0.12-SNAPSHOT.jar server ..\\config-example.yml");
+}
+
